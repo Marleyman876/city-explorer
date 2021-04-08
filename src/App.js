6fd70a9d1f6978992fe82a786f68b1ab9fb0e393
Marleyman876 <grant_garfield@yahoo.com>
2021-04-07 22:41:16 -0700

    got map to render to page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import axios from 'axios';
 
 import Searching from "./Searching.js"
+import City from './City.js';
 
 import './App.css'
-import City from './City.js';
 
 
 class App extends React.Component {
@@ -15,9 +15,7 @@ class App extends React.Component {
     this.state = {
       alreadySearched: false,
       searchedLocation: '',
-      display_name: '',
-      lat:'',
-      lon:'',
+      cityData: {},
     };
   }
   showSearch = () => {
@@ -27,21 +25,19 @@ class App extends React.Component {
   searchHandle = async(searchedLocation)=>{
     let locationData = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${searchedLocation}&format=json`)
     this.setState({
-      lat: locationData.data[0].lat,
-      lon:locationData.data[0].lon,
-      display_name:locationData.data[0].display_name,
+      cityData: locationData.data[0]
+      
 
     })
-
-    console.log(locationData);
   }
 
   render() {
+    console.log(this.state)
     return (
       <>
         <h1>Euro-Trotter</h1>
         <Searching submitButtonEvent={this.searchHandle} />
-        <City Longitude={this.state.lon} Latitude={this.state.lat} display_name={this.state.display_name} />
+        <City display={this.state.cityData} map={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10`}/>
       </>
     )
   };
--- src/City.js
+++ src/City.js
@@ -7,8 +7,8 @@ class City extends React.Component {
     return (
       <>
         {/* <button clickButton={this.props.showSearch }>Search Again</button>*/}
-        <h2>{this.props.display_name}</h2>
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.props.lon}, ${this.props.lat}&zoom=9`} alt="City Map"/>
+        <h2>{this.props.display.display_name}</h2>
+        <img src={this.props.map} alt="City Map"/>
       </>
     )
   }
