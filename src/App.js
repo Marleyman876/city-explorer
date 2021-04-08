db5f9ae09e501c4799aae6201404029ff2a09941
Marleyman876 <grant_garfield@yahoo.com>
2021-04-08 14:12:43 -0700

    fixi netlify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     this.state = {
       alreadySearched: false,
       searchedLocation: '',
-      cityData: {},
+      cityData: {}, 
     };
   }
   showSearch = () => {
