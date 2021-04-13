5d232606004e35b7a081634861a5402c1e8255e4
Marleyman876 <grant_garfield@yahoo.com>
2021-04-12 23:26:44 -0700

    fixed weather

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 
 import Searching from "./Searching.js"
 import City from './City.js';
+import Weather from "./Weather.js"
 
 import './App.css'
 
@@ -19,6 +20,8 @@ class App extends React.Component {
       searchedLocation: '',
       cityData: {},
       errorMessage: false,
+      weatherData: [],
+      displayData: '',
     };
   }
   showSearch = () => {
@@ -27,47 +30,63 @@ class App extends React.Component {
 
   searchHandle = async (searchedLocation) => {
     try {
-      let locationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${searchedLocation}&format=json`)
-      this.setState({
-        cityData: locationData.data[0]
+      let locationData = axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${searchedLocation}&format=json`)
+      .then(location => {
+        this.setState({
+          cityData: location.data[0]
+        });
+        
       })
+      await this.weatherServer();
+      
     } catch (error) {
       this.setState({ errorMessage: error.message })
     }
   };
 
-  weatherServer = async()=>{
-    let weatherData = await axios.get(`${process.env.REACT_APP_BACKEND}/city-weather`);
-    console.log(weatherData);
+  weatherServer = async () => {
+       axios.get(`${process.env.REACT_APP_BACKEND}/city_weather`)
+    .then(weatherData => {
+      // this.setState({
+      //   weatherData: weatherData.data
+      // })
+      console.log(weatherData.data)
+     this.setState({displayData: this.displayWeather(weatherData.data)})
 
+    })
   }
 
-  refreshPage=() =>{
+  displayWeather = (weatherData) => weatherData.map(item => <h1> {item.description}</h1>)
+
+  refreshPage = () => {
     window.location.reload();
   }
 
 
   render() {
-    if(this.state.errorMessage){
-      return(
+    console.log(this.state.displayData)
+    console.log(this.state.errorMessage)
+    if (this.state.errorMessage) {
+      return (
         <>
-        <Alert variant="secondary">
-          <Alert.Heading>Does that City Really Exist?</Alert.Heading>
-          <p>
-            Please renter a real City.
+          <Alert variant="secondary">
+            <Alert.Heading>Does that City Really Exist?</Alert.Heading>
+            <p>
+              Please renter a real City.
           </p>
-        </Alert>
-        <Button variant="outline-light" size="sm" type="button" onClick={this.refreshPage}>Try again!</Button>
+          </Alert>
+          <Button variant="outline-light" size="sm" type="button" onClick={this.refreshPage}>Try again!</Button>
+
         </>
       )
     }
-    
     return (
       <>
         <h1>Euro-Trotter</h1>
-          <Searching submitButtonEvent={this.searchHandle} />
-          <City display={this.state.cityData} />
-
+        <Searching submitButtonEvent={this.searchHandle} />
+        <City display={this.state.cityData} />
+        {/* {this.state.displayData} */}
+        <Weather data={this.state.displayData}/>
 
       </>
     )
